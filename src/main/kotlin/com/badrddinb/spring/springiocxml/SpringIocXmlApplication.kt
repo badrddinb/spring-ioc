ae70d44c67f0c2ae4bebeedc4179eceaed750cce
badreddine bendriss <badrddin.bendriss@gmail.com>
2021-09-25 23:38:27 +0100

    init spring

---
+++ src/main/kotlin/com/badrddinb/spring/springiocxml/SpringIocXmlApplication.kt
@@ -0,0 +1,11 @@
+package com.badrddinb.spring.springiocxml
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringIocXmlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringIocXmlApplication>(*args)
+}
---
+++ src/test/kotlin/com/badrddinb/spring/springiocxml/SpringIocXmlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.badrddinb.spring.springiocxml
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringIocXmlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
