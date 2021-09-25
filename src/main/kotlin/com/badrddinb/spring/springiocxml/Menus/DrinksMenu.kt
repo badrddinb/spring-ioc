b765572e81617de948d92aea10d9e3ce7aa394ca
badreddine bendriss <badrddin.bendriss@gmail.com>
2021-09-26 00:13:07 +0100

    Update Menu bean config object

---
+++ src/main/kotlin/com/badrddinb/spring/springiocxml/Menus/DrinksMenu.kt
@@ -0,0 +1,9 @@
+package com.badrddinb.spring.springiocxml.Menus
+
+import com.badrddinb.spring.springiocxml.Menu
+
+class DrinksMenu: Menu {
+    override fun getItem(): String {
+        return "Drinks item"
+    }
+}
